0f72f768c078aa34fb21df108b1dbc9eb87eb38a
TanmayTrivedi <tanmayrishu1112@gmail.com>
2025-04-26 13:32:11 +0530

    ninth comit

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -37,9 +37,9 @@ const projects = [
 
 const ProjectsSection = () => {
         return (
-          <section className="py-20 px-6 bg-[#393E46] text-white">
-            <h2 className="text-3xl font-bold mb-8 text-center">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+          <section className="py-20 px-6 bg-[#393E46] text-white ">
+            <h2 className="text-3xl font-bold mb-8 text-center ">Projects</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto ">
         {projects.map((project, index) => (
           <div
             key={index}
@@ -47,7 +47,7 @@ const ProjectsSection = () => {
               {project.title === 'Stylish Project Card'
                 ? 'bg-[#00ADB5] text-white'
                 : 'bg-[#222831] text-white'}
-              p-6 rounded-xl border-[3px] border-[#80f0e9] hover:bg-[#393E46] shadow-lg hover:scale-105 transition-transform duration-300`}
+              p-6 rounded-xl border-[3px] border-[#80f0e9] hover:bg-[#80f0e9] hover:text-[#222831] shadow-lg hover:scale-105 transition-transform duration-300`}
           >
             <h3 className="text-xl font-bold mb-2">{project.title}</h3>
             <p className="text-sm text-gray-100 mb-4">{project.description}</p>
