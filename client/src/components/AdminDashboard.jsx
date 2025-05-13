50f563fc02bc9a0699053fa2c04cf6cb6db2c1ab
TanmayTrivedi <tanmayrishu1112@gmail.com>
2025-05-13 23:58:52 +0530

    48th comit

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -6,28 +6,26 @@ const AdminDashboard = ({ setActiveSection }) => {
   const [activeTab, setActiveTab] = useState('blog');
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4 pt-28">Admin Dashboard</h1>
+    <div className="pt-20 bg-[#222831] ">
+      <h1 className="text-full l font-bold mb-4 text-white">Admin Dashboard</h1>
 
-      <div className="flex space-x-4 mb-6 ">
+      <div className="flex space-x-4 mb-6 bg-[#222831]">
         <button
-          className={`px-4 py-2 rounded ${
-            activeTab === 'blog' ? 'bg-cyan-500 text-white' : 'bg-gray-700 text-gray-300'
-          }`}
+          className={"px-6 py-2 border-[3px] border-[#80f0e9] bg-[#222831] text-white font-semibold rounded-full shadow hover:bg-[#80f0e9] hover:text-[#222831] transition hover:scale-105 transition-transform duration-300"
+
+          }
           onClick={() => setActiveTab('blog')}
         >
           Blog Admin
         </button>
         <button
-          className={`px-4 py-2 rounded ${
-            activeTab === 'project' ? 'bg-cyan-500 text-white' : 'bg-gray-700 text-gray-300'
-          }`}
+          className={"px-6 py-2 border-[3px] border-[#80f0e9] bg-[#222831] text-white font-semibold rounded-full shadow hover:bg-[#80f0e9] hover:text-[#222831] transition hover:scale-105 transition-transform duration-300"}
           onClick={() => setActiveTab('project')}
         >
           Project Admin
         </button>
         <button
-          className="ml-auto px-4 py-2 bg-red-600 text-white rounded"
+          className="px-6 py-2 border-[3px] border-[#80f0e9] bg-[#222831] text-white font-semibold rounded-full shadow hover:bg-[#80f0e9] hover:text-[#222831] transition hover:scale-105 transition-transform duration-300"
           onClick={() => setActiveSection('home')}
         >
           Exit Admin
--- client/src/components/AdminProject.jsx
+++ client/src/components/AdminProject.jsx
@@ -3,11 +3,11 @@ import { uploadProjectsToFirebase } from '../utils/UploadProject'; // adjust pat
 
 const AdminProjectPage = () => {
   return (
-    <div className="p-6 text-white">
-      <h2 className="text-2xl font-bold mb-4">Admin Project Uploader</h2>
+    <div className="bg-[#222831] min-h-screen">
+      <h2 className="text-2xl text-[#80f0e9] font-bold mb-4">Admin Project Uploader</h2>
       <button
         onClick={uploadProjectsToFirebase}
-        className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded"
+        className="px-6 py-2 border-[3px] border-[#80f0e9] bg-[#222831] text-white font-semibold rounded-full shadow hover:bg-[#80f0e9] hover:text-[#222831] transition hover:scale-105 transition-transform duration-300"
       >
         Upload Projects to Firebase
       </button>
